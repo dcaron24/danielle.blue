b2fabddb747a2d91dc2f2789a3aeece9cb33332f
Danielle Caron <Danielle@MacBook-Pro.local>
2019-01-02 21:09:13 -0800

    added scroll to topbar via headroom

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
+import Headroom from 'react-headroom';
 
 import './index.css';
 
 export default class TopBar extends Component {
   render() {
     return (
-      <div className='top-bar-container'>
-        <div
-          onClick={() => window.location.replace('/')}
-          className={`top-bar-text ${this.props.active === '/' ? 'active' : ''}`}>
-          {'Home'}
+      <Headroom>
+        <div className='top-bar-container'>
+          <div
+            onClick={() => window.location.replace('/')}
+            className={`top-bar-text ${this.props.active === '/' ? 'active' : ''}`}>
+            {'Home'}
+          </div>
+          <div
+            onClick={() => window.location.replace('/about')}
+            className={`top-bar-text ${this.props.active === '/about' ? 'active' : ''}`}>
+            {'About'}
+          </div>
         </div>
-        <div
-          onClick={() => window.location.replace('/about')}
-          className={`top-bar-text ${this.props.active === '/about' ? 'active' : ''}`}>
-          {'About'}
-        </div>
-      </div>
+      </Headroom>
     );
   }
 }
