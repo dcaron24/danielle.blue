51ee2504ed56e6fd234f639f0a9eabaf7b2461ea
Danielle Caron <Danielle@MacBook-Pro.local>
2019-01-06 15:28:47 -0800

    switched home and gallery content to be on the correct pages

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+// imported libraries
 
 import TopBar from '../components/top-bar';
 import PageHeader from '../components/page-header';
+import ImageBox from '../components/image-box';
+// imported files
+// .. goes back a folder
 
 export default class Gallery extends Component {
   render() {
-    console.log(this.props.location.pathname);
-
+    // print out something with console.log,
+    // such as this component's properites
+    // always take out logs because they get uploaded
+    // console.log(this.props.location.pathname);
     return (
+      // html begins here, it is mixed with javascript
       <div className='container-background'>
+        {/* active is a custom property, it is made up */}
         <TopBar active={this.props.location.pathname} />
-        <PageHeader text='Gallery!!' />
+        {/* brackets mean variables */}
+        <PageHeader text='danielle.blue coming soon...' />
+
+        <div className='home-page-content'>
+          <ImageBox image='https://yt3.ggpht.com/a-/ACSszfFE3ueNAPUAV9ybUfRL0pE9e05zQ05e9y2c1A=s900-mo-c-c0xffffffff-rj-k-no'/>
+          <ImageBox image='https://i.ytimg.com/vi/qsxcVsFDDoA/maxresdefault.jpg'/>
+          <ImageBox image='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/BackSmithGrind.jpg/220px-BackSmithGrind.jpg'/>
+          <ImageBox image='https://yt3.ggpht.com/a-/ACSszfFE3ueNAPUAV9ybUfRL0pE9e05zQ05e9y2c1A=s900-mo-c-c0xffffffff-rj-k-no'/>
+          <ImageBox image='https://i.ytimg.com/vi/qsxcVsFDDoA/maxresdefault.jpg'/>
+          <ImageBox image='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/BackSmithGrind.jpg/220px-BackSmithGrind.jpg'/>
+        </div>
       </div>
     );
   }
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-// imported libraries
 
 import TopBar from '../components/top-bar';
 import PageHeader from '../components/page-header';
-import ImageBox from '../components/image-box';
-// imported files
-// .. goes back a folder
 
 export default class Home extends Component {
   render() {
-    // print out something with console.log,
-    // such as this component's properites
-    // always take out logs because they get uploaded
-    // console.log(this.props.location.pathname);
-    return (
-      // html begins here, it is mixed with javascript
-      <div className='container-background'>
-        {/* active is a custom property, it is made up */}
-        <TopBar active={this.props.location.pathname} />
-        {/* brackets mean variables */}
-        <PageHeader text='danielle.blue coming soon...' />
+    console.log(this.props.location.pathname);
 
-        <div className='home-page-content'>
-          <ImageBox image='https://yt3.ggpht.com/a-/ACSszfFE3ueNAPUAV9ybUfRL0pE9e05zQ05e9y2c1A=s900-mo-c-c0xffffffff-rj-k-no'/>
-          <ImageBox image='https://i.ytimg.com/vi/qsxcVsFDDoA/maxresdefault.jpg'/>
-          <ImageBox image='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/BackSmithGrind.jpg/220px-BackSmithGrind.jpg'/>
-          <ImageBox image='https://yt3.ggpht.com/a-/ACSszfFE3ueNAPUAV9ybUfRL0pE9e05zQ05e9y2c1A=s900-mo-c-c0xffffffff-rj-k-no'/>
-          <ImageBox image='https://i.ytimg.com/vi/qsxcVsFDDoA/maxresdefault.jpg'/>
-          <ImageBox image='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/BackSmithGrind.jpg/220px-BackSmithGrind.jpg'/>
-        </div>
+    return (
+      <div className='container-background'>
+        <TopBar active={this.props.location.pathname} />
+        <PageHeader text='Gallery!!' />
       </div>
     );
   }
