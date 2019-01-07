4909f2cdb5b9f3bc42148f218a3e605c1ffa3706
Danielle Caron <Danielle@MacBook-Pro.local>
2019-01-06 17:31:48 -0800

    added some fonts, made home page paragraph

--- src/components/home-page-content/index.js
+++ src/components/home-page-content/index.js
@@ -7,6 +7,11 @@ import './index.css';
 const HomePageContent = (props) => {
   return(
     <div className='home-page-content'>
+
+      <div className='home-page-text'>
+        {'Hello blah blah blah'}
+        {/* <span>dfgdfgdfg</span> */}
+      </div>
       <Button
         text='Gallery'
         onClick={() => console.log('Hello')} />
