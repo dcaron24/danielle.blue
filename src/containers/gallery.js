925a643ebfbe736e885503872a970fdeba17a8c3
Danielle Caron <Danielle@MacBook-Pro-5.local>
2019-12-01 22:13:23 -0700

    added moment and image date sorter

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 // imported libraries
 
 import TopBar from '../components/top-bar';
@@ -8,6 +9,18 @@ import ImageBox from '../components/image-box';
 import galleryImages from '../gallery-images.js';
 // imported files
 // .. goes back a folder
+
+galleryImages.sort((a, b) => {
+  const A = moment(a.date)
+  const B = moment(b.date)
+  if (A < B) {
+    return 1
+  } else if (B < A) {
+    return -1
+  } else {
+    return 0
+  }
+});
 
 export default class Gallery extends Component {
   render() {
