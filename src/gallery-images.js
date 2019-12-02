738ba4467c28c4d27d365d7daa6b7f5f087de82b
Danielle Caron <Danielle@MacBook-Pro-5.local>
2019-12-02 00:57:20 -0700

    uploaded a shit load of images

--- src/gallery-images.js
+++ src/gallery-images.js
@@ -2,37 +2,505 @@ export default [
   {
     title: 'Cactus Snake Board',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/cactusboard.1.jpg',
-    category: 'board',
+    category: ['board'],
     date: '11-14-2018'
   },
   {
     title: 'Jack the Yorkie',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/jack.1.jpg',
-    category: 'portrait',
+    category: ['pencil','portrait'],
     date: '07-01-2018'
   },
   {
     title: 'Pippie the Poodle',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/poodle.1.jpg',
-    category: 'portrait',
+    category: ['pencil','portrait'],
     date: '10-01-2018'
   },
   {
     title: 'Finley the Frenchie i',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/finley.1.jpg',
-    category: 'drawing',
+    category: ['marker','portrait'],
     date: '12-25-2018'
   },
   {
     title: 'Bella the Frenchie',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/bella.1.jpg',
-    category: 'drawing',
+    category: ['marker','portrait'],
     date: '12-24-2018'
   },
   {
     title: 'Finley the Frenchie ii',
     imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/finley.2.jpg',
-    category: 'drawing',
+    category: ['marker','portrait'],
     date: '12-23-2018'
-  }
+  },
+  {
+    title: 'Alien Head',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/alien+drawing.jpg',
+    category: ['marker'],
+    date: '08-06-2018'
+  },
+  {
+    title: 'Andrew Watercolor',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/andrew+watercolor.jpg',
+    category: ['painting','portrait'],
+    date: '02-25-2019'
+  },
+  {
+    title: 'Andrew Portrait',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/andrew+portrait.jpg',
+    category: ['portrait','pencil'],
+    date: '06-26-2018'
+  },
+  {
+    title: 'Bell Pepper Oil Painting',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/bell+pepper+oil.jpg',
+    category: ['painting'],
+    date: '10-17-2014'
+  },
+  {
+    title: 'Black Flower Board',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/black+and+white+flower+board.jpg',
+    category: ['board'],
+    date: '04-27-2018'
+  },
+  {
+    title: 'Blue Flower Board',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/blue+flower+board.jpg',
+    category: ['board'],
+    date: '04-23-2018'
+  },
+  {
+    title: 'Brown Pattern',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/brown+pattern.jpg',
+    category: ['marker'],
+    date: '05-27-2018'
+  },
+  {
+    title: 'Hammerhead Bunny',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/bunny+foo+foo.jpg',
+    category: ['pencil'],
+    date: '05-09-2016'
+  },
+  {
+    title: 'Bweano Portrait',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/bweano.jpg',
+    category: ['portrait','marker'],
+    date: '08-22-2019'
+  },
+  {
+    title: 'Cara Watercolor',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/cara+watercolor.jpg',
+    category: ['portrait','painting'],
+    date: '09-04-2019'
+  },
+  {
+    title: 'Doodles',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/carpet+monsters.jpg',
+    category: ['marker'],
+    date: '10-02-2017'
+  },
+  {
+    title: 'Catastrophe Cut Paper',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/catastrophe+cut+paper.jpg',
+    category: ['mixed'],
+    date: '10-06-2014'
+  },
+  {
+    title: 'Cherry Watercolor',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/cherry+watercolor.jpg',
+    category: ['painting'],
+    date: '01-09-2019'
+  },
+  {
+    title: 'Chloe Grace Moretz',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/chloe+moretz.jpg',
+    category: ['portrait','mixed'],
+    date: '06-26-2014'
+  },
+  {
+    title: 'Danielle Portrait',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/danielle+portrait+pencil.jpg',
+    category: ['portrait','pencil'],
+    date: '07-24-2018'
+  },
+  {
+    title: 'Danielle Portrait',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/danielle+portrait.jpg',
+    category: ['portrait','marker'],
+    date: '11-04-2019'
+  },
+  {
+    title: 'Danielle Watercolor',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/danielle+watercolor.jpg',
+    category: ['portrait','painting'],
+    date: '02-21-2019'
+  },
+  {
+    title: 'Emily Portrait',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/emily.jpg',
+    category: ['portrait','pencil'],
+    date: '09-02-2015'
+  },
+  {
+    title: 'Happy Fathers Day',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/fathersday.1.jpg',
+    category: ['mixed','portrait'],
+    date: '06-23-2014'
+  },
+  {
+    title: 'Happy Fathers Day Watercolor',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/fathersday.2.jpg',
+    category: ['painting'],
+    date: '06-15-2019'
+  },
+  {
+    title: 'Feather Board',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/feather+board.jpg',
+    category: ['board'],
+    date: '06-22-2018'
+  },
+  {
+    title: 'Tri Fish',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/fish.jpg',
+    category: ['painting','pencil'],
+    date: '06-14-2014'
+  },
+  {
+    title: 'Flower Doodle',
+    imageUrl: 'https://s3-us-west-1.amazonaws.com/images.danielle.blue/flower+pattern.jpg',
+    category: ['marker'],
+    date: '09-04-2018'
+  },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // },
+  // {
+  //   title: '',
+  //   imageUrl: '',
+  //   category: [''],
+  //   date: '--20'
+  // }
 ]
